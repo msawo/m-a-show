ef7a987877714e7078d4d9fccfec64bb04dda938
msawo <msawo.inc@gmail.com>
2020-05-12 01:26:38 -0400

    Add Player page, Listview of Episodes widgets && parse rssFeed

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_sound/flutter_sound.dart';
+import 'package:webfeed/webfeed.dart';
+import 'package:http/http.dart' as http;
 
 final url = 'https://itsallwidgets.com/podcast/feed';
 void main() => runApp(MandAShow());
@@ -18,15 +20,73 @@ class EpisodesPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(),
+      body: FutureBuilder(
+        future: http.get(url),
+        builder: (context, AsyncSnapshot<http.Response> snapshot) {
+          if (snapshot.hasData) {
+            final response = snapshot.data;
+            if (response.statusCode == 200) {
+              final rssString = response.body;
+              var rssFeed = RssFeed.parse(rssString); // for parsing RSS feed
+              return EpisodeListView(rssFeed: rssFeed);
+            }
+          } else {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+        },
+      ),
+    );
+  }
+}
+
+class EpisodeListView extends StatelessWidget {
+  const EpisodeListView({
+    Key key,
+    @required this.rssFeed,
+  }) : super(key: key);
+
+  final RssFeed rssFeed;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: rssFeed.items
+          .map(
+            (i) => ListTile(
+              title: Text(i.title),
+              subtitle: Text(
+                i.description,
+                maxLines: 3,
+                overflow: TextOverflow.ellipsis,
+              ),
+              onTap: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (_) => PlayerPage(item: i),
+                  ),
+                );
+              },
+            ),
+          )
+          .toList(),
     );
   }
 }
 
 class PlayerPage extends StatelessWidget {
+
+  final RssItem item;
+
+  PlayerPage({this.item});
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text(item.title),
+
+      ),
       body: SafeArea(
         child: Player(),
       ),
